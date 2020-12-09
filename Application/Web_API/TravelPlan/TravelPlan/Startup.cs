53175139b1e8f10fb5013fd3840bd750114dd131
trifunovicPetar98 <peki.trifunovic@gmail.com>
2020-12-09 02:09:04 +0100

    Installed EntityFramework packages in the API project. Added connection string for the remotely hosted Azure database. Updated the Startup.cs file.

--- Application/Web_API/TravelPlan/TravelPlan/Startup.cs
+++ Application/Web_API/TravelPlan/TravelPlan/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using TravelPlan.DataAccess;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -26,6 +28,23 @@ namespace TravelPlan
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                services.AddDbContext<TravelPlanDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProdConnString")));
+            }
+            else if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            {
+                services.AddDbContext<TravelPlanDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DevelopConnString")));
+            }
+
+            //NOTE: The following lines can be used to apply migrations during runtime.
+            //TODO: Check if this code has any purpose when creating migrations in the DataAccess class library.
+
+            //ServiceProvider sp = services.BuildServiceProvider();
+            //WeatherForecastContext wfc = sp.GetService<WeatherForecastContext>();
+            //wfc.Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
