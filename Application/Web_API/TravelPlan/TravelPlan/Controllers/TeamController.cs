231565f8a90c23490d1f020b63edcabc36d1b329
petarTrifunovic98 <peki.trifunovic@gmail.com>
2021-01-08 00:07:28 +0100

    Added User and Team controllers

---
+++ Application/Web_API/TravelPlan/TravelPlan/Controllers/TeamController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelPlan.Contracts.ServiceContracts;
+using TravelPlan.DTOs.DTOs;
+
+namespace TravelPlan.API.Controllers
+{
+    [ApiController]
+    [Route("api/team")]
+    public class TeamController : ControllerBase
+    {
+        private readonly ITeamService _teamService;
+        public TeamController(ITeamService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        [HttpPost]
+        [Route("creator/{creatorId}/create-team")]
+        public async Task<ActionResult> CreateTeam(int creatorId, [FromBody]CreateTeamDTO newTeam)
+        {
+            try
+            {
+                if (await _teamService.CreateTeam(creatorId, newTeam))
+                    return Ok();
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("get-teams")]
+        public async Task<ActionResult> GetTeams()
+        {
+            IEnumerable<TeamDTO> teams = await _teamService.GetTeams();
+            return Ok(teams);
+        }
+    }
+}
---
+++ Application/Web_API/TravelPlan/TravelPlan/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelPlan.Contracts.ServiceContracts;
+using TravelPlan.DTOs.DTOs;
+
+namespace TravelPlan.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("add-account")]
+        public async Task<ActionResult> AddUserAccount(UserRegisterDTO userInfo)
+        {
+            try
+            { 
+                if(await _userService.AddUserAccount(userInfo))
+                    return Ok();
+                return BadRequest();
+            }
+            catch(Exception ex)
+            { 
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
