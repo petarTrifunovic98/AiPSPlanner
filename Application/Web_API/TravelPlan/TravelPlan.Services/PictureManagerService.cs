ef4ed5786a7351027ad6e6b80752a91dc44fc081
petarTrifunovic98 <peki.trifunovic@gmail.com>
2021-01-09 02:45:28 +0100

    Added picture manager service for saving and loading images in base64 representation.

---
+++ Application/Web_API/TravelPlan/TravelPlan.Services/PictureManagerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TravelPlan.Services
+{
+    public class PictureManagerService
+    {
+        public static void SaveImageToFile(string base64Image, string path)
+        {
+            byte[] bytes = Convert.FromBase64String(base64Image);
+            File.WriteAllBytes(path, bytes);
+        }
+
+        public static string LoadImageFromFile(string path)
+        {
+            string base64Image = "";
+            if (!string.IsNullOrEmpty(path))
+            {
+                byte[] bytes = File.ReadAllBytes(path);
+                base64Image = Convert.ToBase64String(bytes);
+            }
+            return base64Image;
+        }
+    }
+}
