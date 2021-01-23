591ed88b3ee7c2d5469c08a7d639daf4a181a4a1
petarTrifunovic98 <peki.trifunovic@gmail.com>
2021-01-23 07:56:26 +0100

    SignalR hub added.

---
+++ Application/Web_API/TravelPlan/TravelPlan.Services/MessagingService/IMessageHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlan.Services.MessagingService
+{
+    public interface IMessageHub
+    {
+        Task TestMethod(string testMessage);
+    }
+}
---
+++ Application/Web_API/TravelPlan/TravelPlan.Services/MessagingService/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TravelPlan.Services.MessagingService
+{
+    public class MessageHub : Hub<IMessageHub>
+    {
+        public async Task TestHubMethod(string message)
+        {
+            //TestMethod - metoda IMessageHub interfejsa
+            await Clients.All.TestMethod(message);
+        }
+    }
+}
